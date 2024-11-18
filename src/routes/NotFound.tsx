d2872abc3c08058f7bddf3b8af62777200406204
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2024-11-19 01:07:48 +0200

    style: change 404 page styles and add 404 image

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,18 +1,39 @@
-import { cn } from '@utils';
 import { House } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
+import { Img } from '@components/ui/image';
+
+import { cn } from '@utils';
+
+import notFound from '@assets/images/404.png';
+
 export const NotFound = () => {
   return (
-    <div className="mt-52 flex flex-col items-center font-semibold">
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+    <div
+      className={cn(
+        'flex flex-col items-center justify-center',
+        'h-screen w-screen p-10',
+        'text-center font-semibold',
+        'bg-blue-light'
+      )}
+    >
+      <div className="h-[400px] w-[400px]">
+        <Img
+          src={notFound}
+          alt="404 - not found"
+        />
+      </div>
+      <h1 className="mb-2 text-[18px] font-medium">
+        The page you are looking for does not exist.
+      </h1>
       <Link
         className={cn(
-          'inline-flex gap-2 rounded-full px-4 py-3 mt-4',
-          'bg-violet-400 text-left text-sm font-medium text-white',
+          'inline-flex items-center gap-2',
+          'mt-4 h-[60px] px-4',
+          'text-left text-sm font-medium',
+          'rounded-full bg-black text-white',
           'transition-all duration-100 ease-out',
-          'hover:bg-violet-300'
+          'hover:bg-black-dark'
         )}
         to="/"
         replace
