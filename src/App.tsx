493b1c8e154691c8a79637d1ffc14747cc4a8453
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2024-11-19 00:08:31 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { AppProvider } from '@AppProvider'; 
+import { AppProvider } from '@AppProvider';
 import { router } from '@routes';
+
+const BASE_NAME =
+  import.meta.env.MODE === 'production' ? '/tic-tac-toe' : '/';
 
 function App() {
   return (
     <AppProvider>
-      <RouterProvider router={router} />
+      <BrowserRouter basename={BASE_NAME}>
+        <RouterProvider router={router} />
+      </BrowserRouter>
     </AppProvider>
   );
 }
